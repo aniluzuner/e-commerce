5bbceb173d6691ba8a6182f81592169d74299ee6
aniluzuner <anil.uzuner@outlook.com>
2024-01-09 14:08:18 +0300

    Created Login Component

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Navbar from '/components/navbar';
 import Carousel from '/components/carousel';
+import Login from '/components/login';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         "Images/carousel-image.jpg",
         "Images/carousel-image.jpg",
         "Images/carousel-image.jpg"]} />
+      <Login />
     </>
     
   )
